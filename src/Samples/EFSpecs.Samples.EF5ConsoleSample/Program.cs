d1f18133c3ed9ad2457287ee6b5a6a160211f09b
Kris McGinnes <kmcginnes@me.com>
2013-03-15 14:50:17 -0500

    Added sample application

---
+++ src/Samples/EFSpecs.Samples.EF5ConsoleSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFSpecs.Samples.EF5ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
