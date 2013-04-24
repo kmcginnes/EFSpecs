1cfa9b236b0d45ffb822c1c02ae54e0078f02dcf
Kris McGinnes <kmcginnes@me.com>
2013-04-24 11:55:20 -0500

    Updated AssemblyInfo for release

--- src/EFSpecs/Properties/AssemblyInfo.cs
+++ src/EFSpecs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EFSpecs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EFSpecs is a library to assist in testing your entity framework model mapping")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fjord")]
 [assembly: AssemblyProduct("EFSpecs")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Fjord 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
